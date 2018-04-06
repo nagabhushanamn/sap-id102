013399464e5bc11e9dcc69113c642e9e4916a4a9
Nagabhushanam <nagabhushanamn@gmail.com>
2018-04-06 09:22:38 +0530

    shop app

--- 2-React/3-shop-v1/demo-app/src/App.js
+++ 2-React/3-shop-v1/demo-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import classnames from 'classnames';
 
 class App extends Component {
   constructor(props) {
@@ -30,30 +31,24 @@ class App extends Component {
   changeTab(tab) {
     this.setState({ tab });
   }
+
   renderTabCard(product) {
     let { tab } = this.state;
+    let card;
     switch (tab) {
       case 1:
-        return (
-          <div className="card">
-            <div className="card-body">{product.description}</div>
-          </div>
-        );
+        card = (<div>{product.description}</div>)
+        break;
       case 2:
-        return (
-          <div className="card">
-            <div className="card-body">Not Yet</div>
-          </div>
-        );
+        card = (<div>Not Yet</div>)
+        break;
       case 3:
-        return (
-          <div className="card">
-            <div className="card-body">None Yet</div>
-          </div>
-        )
+        card = <div>None Yet</div>
+        break;
       default:
-        return null;
+        card = null;
     }
+    return card;
   }
 
   renderBuyBtn(product) {
@@ -64,7 +59,7 @@ class App extends Component {
     }
   }
   renderProducts() {
-    let { products } = this.state;
+    let { products,tab } = this.state;
     return products.map((product, idx) => {
       return (
         <div className="list-group-item" key={idx}>
@@ -78,9 +73,9 @@ class App extends Component {
               {this.renderBuyBtn(product)}
               <hr />
               <ul className="nav nav-tabs">
-                <li className="nav-item"><a onClick={() => { this.changeTab(1) }} className="nav-link" href>Description</a></li>
-                <li className="nav-item"><a onClick={() => { this.changeTab(2) }} className="nav-link" href>Specification</a></li>
-                <li className="nav-item"><a onClick={() => { this.changeTab(3) }} className="nav-link" href>Reviews</a></li>
+                <li className="nav-item"><a onClick={() => { this.changeTab(1) }} className={classnames({'nav-link':true,'active':tab===1})} href>Description</a></li>
+                <li className="nav-item"><a onClick={() => { this.changeTab(2) }} className={classnames({'nav-link':true,'active':tab===2})} href>Specification</a></li>
+                <li className="nav-item"><a onClick={() => { this.changeTab(3) }} className={classnames({'nav-link':true,'active':tab===3})} href>Reviews</a></li>
               </ul>
               {this.renderTabCard(product)}
             </div>
